85ba849392252bc9dcd7c830442786fb0bfb6986
Çağlar ERGÜL <caglarergul@me.com>
2020-05-26 05:10:20 +0300

    Woocommerce is added and platform icons are uploaded.

--- index.js
+++ index.js
@@ -13,16 +13,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let isItShopify = false;
-let isItBigCommerce = false;
-let isItVolusion = false;
-let isItMagento = false;
-let isIt3dCart = false;
-
 let thePlatformObject = {
     website: {
         platform: null,
-        url: null
+        url: null,
+        platform_icon: null
     },
     marketing: {
         facebook_pixel_exist: null,
@@ -38,12 +33,28 @@ checkFacebookPixel = (html) => {
     }
 }
 
+
+
 checkGoogleAnalytics = (html) => {
     if (html.includes("google-analytics") || html.includes("GoogleAnalytics") || html.includes("UA-")) {
         thePlatformObject.marketing.google_analytics_exist = true;
     } else {
         thePlatformObject.marketing.google_analytics_exist = false;
     }
+}
+
+checkWooCommerceMetaContent = (html) => {
+    let metaTagLength = $('meta', html).length;
+
+    const metaContent = [];
+    for (let i = 0; i < metaTagLength; i++) {
+        //req.json($('script', html).json());
+        if ($('meta', html)[i].attribs.content !== undefined) {
+            metaContent.push($('meta', html)[i].attribs.content);
+            // console.log(metaContent);
+        }
+    }
+    return metaContent;
 }
 
 checkScriptsToIdentifyPlatform = (html) => {
@@ -54,6 +65,7 @@ checkScriptsToIdentifyPlatform = (html) => {
         //req.json($('script', html).json());
         if ($('script', html)[i].attribs.src !== undefined) {
             scriptSrc.push($('script', html)[i].attribs.src);
+            // console.log(scriptSrc);
         }
     }
     return scriptSrc;
@@ -61,7 +73,11 @@ checkScriptsToIdentifyPlatform = (html) => {
 
 
 app.post('/platform', (req, res) => {
-    console.log("URL:", req.body.url)
+    let isPlatformRecognized = false;
+    let isItUnKnownPlatform = false;
+    let wordpressIsFound = false;
+    console.log("URL:", req.body.url);
+
     rp({
         url: req.body.url,
         headers: {
@@ -71,67 +87,70 @@ app.post('/platform', (req, res) => {
         thePlatformObject.website.url = req.body.url;
         checkFacebookPixel(html);
         checkGoogleAnalytics(html);
-        return checkScriptsToIdentifyPlatform(html);
-    }).then((data) => {
-        data.map((item) => {
-            if (item.includes("cdn.shopify.com")) {
-                //console.log("storefront is found");
-                isItShopify = true;
-                isItUnKnownPlatform = false;
-                thePlatformObject.website.platform = "Shopify";
-            } else if (item.includes(".bigcommerce.com")) {
-                //console.log("storefront is found");
-                isItBigCommerce = true;
-                isItUnKnownPlatform = false;
-                thePlatformObject.website.platform = "Bigcommerce";
-            } else if (item.includes("vspfiles")) {
-                //console.log("storefront is found");
-                isItVolusion = true;
-                isItUnKnownPlatform = false;
-                thePlatformObject.website.platform = "Volusion";
-            } else {
-                if (isItShopify || isItBigCommerce ||  isItVolusion) {
-                    isItUnKnownPlatform = false;
-                }else {
-                    isItShopify = false;
-                    isItBigCommerce = false;
-                    isItVolusion = false;
-                    isItUnKnownPlatform = true;
-                }
-
-                
+        let wooCommerceMeta = checkWooCommerceMetaContent(html);
+        wooCommerceMeta.map((item) => {
+            if (String(item.match(/woocommerce/gi)) === "WooCommerce") {
+                isPlatformRecognized = true;
+                wordpressIsFound = true;
+                thePlatformObject.website.platform = "WooCommerce";
+                thePlatformObject.website.platform_icon = "/platform_icons/woocommerce-icon.png";
+                console.log("This website is WooCommerce");
+                res.json(thePlatformObject);
             }
         });
-    }).then(() => {
-        if (isItShopify) {
-            console.log("This website is Shopify");
-            res.json(thePlatformObject);
+if (!isPlatformRecognized && !wordpressIsFound) {
+    let scriptTags = checkScriptsToIdentifyPlatform(html);
+    return scriptTags;
+}else {
+    return false
+}
+        
+    }).then((data) => {
+        if (data) {
+            data.map((item) => {
+
+                if (isPlatformRecognized === false) {
+                    if (item.includes("cdn.shopify.com")) {
+                        isPlatformRecognized = true;
+                        thePlatformObject.website.platform = "Shopify";
+                        thePlatformObject.website.platform_icon = "/platform_icons/shopify-icon.png";
+                        console.log("This website is Shopify");
+                        res.json(thePlatformObject);
+                    } else if (item.includes(".bigcommerce.com")) {
+                        console.log("This website is Bigcommerce");
+                        isPlatformRecognized = true;
+                        thePlatformObject.website.platform = "Bigcommerce";
+                        thePlatformObject.website.platform_icon = "/platform_icons/bigcommerce-icon.png";
+                        res.json(thePlatformObject);
+                    } else if (item.includes("vspfiles")) {
+                        isPlatformRecognized = true;
+                        thePlatformObject.website.platform = "Volusion";
+                        thePlatformObject.website.platform_icon = "/platform_icons/volusion-icon.png";
+                        console.log("This website is Volusion");
+                        res.json(thePlatformObject);
+                    } else {
+                        isPlatformRecognized = true;
+                        thePlatformObject.website.platform = "Standalone Application";
+                        thePlatformObject.website.platform_icon = "/platform_icons/standalone-icon.png";
+                        console.log("This website is Standalone Application");
+                        res.json(thePlatformObject);
+                    }
+                }
+            });
+        }else {
+            console.log("skipped!")
         }
-        else if (isItBigCommerce) {
-            console.log("This website is Bigcommerce");
-            res.json(thePlatformObject);
-        }
-        else if (isItVolusion) {
-            console.log("This website is Volusion");
-            res.json(thePlatformObject);
-        }
-        else if (isItMagento) {
-            console.log("This website is Magento");
-            res.json(thePlatformObject);
-        }
-        else if (isIt3dCart) {
-            console.log("This website is 3DCart Commerce");
-            res.json(thePlatformObject);
-        }
-        else if (isItUnKnownPlatform) {
-            console.log("Unknown Platform");
-            thePlatformObject.website.platform = "Unknown Platform";
-            res.json(thePlatformObject);
-        }
+
+
 
     }).catch(function (err) {
         //handle error
-        //console.log(err)
+        if (err.statusCode === 403) {
+            res.json({error: {status_code : 403, message: "The website is unreachacble or using cloudflare firewall."}});
+        }else {
+            res.json({error: {message: err}});
+        }
+        console.log(err.statusCode)
     });
 
 });
